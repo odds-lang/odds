c0ee7c7f59e1db18bef5931c228a7c31e3d27c34
Alex Kalicki <alexander.kalicki@gmail.com>
2015-10-19 13:44:49 -0400

    #3 - add initial tests, hook bash file up to tokenize

--- test/scanner_test.sh
+++ test/scanner_test.sh
@@ -5,13 +5,13 @@ echo "# Running scanner tests..."
 
 for input_file in $INPUT_FILES; do
     output_file=${input_file/input/output}
-    cat $input_file | cmp -s $output_file -
+    scanner/tokenize < $input_file | cmp -s $output_file -
     if [ "$?" -eq 0 ]; then
-        echo "   - checking $input_file against $output_file: SUCCESS"
+       echo "   - checking $input_file against $output_file: SUCCESS"
     else
-        echo "   - checking $input_file against $output_file: ERROR" 1>&2
-        exit 1
+       echo "   - checking $input_file against $output_file: ERROR" 1>&2
+       exit 1
     fi
-done;
+done
 
 exit 0
