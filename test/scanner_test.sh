9dced63e33394790c83880af10feca776e443176
Alex Kalicki <alexander.kalicki@gmail.com>
2015-10-18 23:08:01 -0400

    write error to stderr

--- test/scanner_test.sh
+++ test/scanner_test.sh
@@ -9,7 +9,7 @@ for input_file in $INPUT_FILES; do
     if [ "$?" -eq 0 ]; then
         echo "   - checking $input_file against $output_file: SUCCESS"
     else
-        echo "   - checking $input_file against $output_file: ERROR"
+        echo "   - checking $input_file against $output_file: ERROR" 1>&2
         exit 1
     fi
 done;
