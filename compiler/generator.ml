9e1a1254d5ae34f9c756ac502db908c20749e46e
dannyech <dannyech@gmail.com>
2015-11-04 22:15:45 -0500

    added exp_to_text to generator.ml and then realized all of this was for naught because we ought to do all the conversions to text in the sast file

--- compiler/generator.ml
+++ compiler/generator.ml
@@ -11,10 +11,12 @@
 open Ast
 open Printf
 
+(* WILL REMOVE AND ADD TO SAST *)
 let func_to_text f arg = match f with
-  | "print" -> sprintf "print(\"%s\")" arg
+  | "print" -> sprintf "print(%s)" arg
   | _ -> "" (* f(args) *)
 
+(* WILL REMOVE AND ADD TO SAST *)
 let op_to_text op = match op with
   | Add -> "+"
   | Sub -> "-"
@@ -23,6 +25,16 @@ let op_to_text op = match op with
   | Mod -> "%"
   | Pow -> "**"
 
+(* WILL REMOVE AND ADD TO SAST *)
+let rec exp_to_text exp = match exp with
+  | Int_lit(i) -> string_of_int(i)
+  | Float_lit(f) -> string_of_float(f)
+  | String_lit(s) -> sprintf "\"%s\"" s
+  | Binop(e1, op, e2) -> 
+      sprintf "%s %s %s" (exp_to_text e1) (exp_to_op op) (exp_to_text e2)
+  | Unop(op, e) -> sprintf "%s %s" (op_to_text op) (exp_to_text e)
+
+(* Entry Point *)
 let py_file = open_out "out.py"
 
 in let rec process_stmt_list stmt_list = match stmt_list with
@@ -31,5 +43,5 @@ in let rec process_stmt_list stmt_list = match stmt_list with
   | [] -> close_out py_file
 
 and process_stmt stmt = match stmt with
-  | State(expr) -> generate_expr(expr)
+  | State(expr) -> ""
 
