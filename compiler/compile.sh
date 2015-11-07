1876ccbcd1eaacc10ae24bb1728d7280a444a012
Alexandra Medway <afm2134@columbia.edu>
2015-11-06 19:42:30 -0500

    Compile script complete. See compile.sh for usage

--- compiler/compile.sh
+++ compiler/compile.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-make clean
-make 
-./odds < $1
+if [ "$#" -ne 3 ] 
+then
+  echo "Use: ./compile [file_to_compile] [flag] [output.py]"
+  exit 1 
+else
+  make clean
+  make 
+  cat $1 | ./odds $2 $3
+fi
--- compiler/odds.ml
+++ compiler/odds.ml
@@ -9,18 +9,16 @@
  *)
 
 type action = Ast | Gen
-
+let lexbuf = String.sub Sys.argv.(2) 0 (String.length(Sys.argv.(2)) - 3)
 let _ =
   let action =
-    if Array.length Sys.argv > 1 then 
-      List.assoc Sys.argv.(1) [("-a", Ast); ("-g", Gen);]
-    else Gen in 
+    List.assoc Sys.argv.(1) [("-a", Ast); ("-g", Gen);] in
     let lexbuf = Lexing.from_channel stdin in 
     let output_file = String.sub Sys.argv.(2) 0 (String.length(Sys.argv.(2)) - 3) in
     let program = Parser.program Scanner.token lexbuf in 
-    match action with
-    | Ast ->
+      match action with
+      | Ast ->
         print_string("Todo, add pretty printer for AST")
-    | Gen ->
+      | Gen ->
         Generator.gen_program output_file program
 
