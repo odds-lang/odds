f24a59b694d25eaed135a2b97f4d75ece56933c7
Alexandra Medway <afm2134@columbia.edu>
2015-11-07 13:25:24 -0500

    Fixed bug, didn't have call

--- compiler/generator.ml
+++ compiler/generator.ml
@@ -19,7 +19,7 @@ let rec txt_of_expr expr = match expr with
   | Binop(e1, op, e2) ->
       sprintf "%s %s %s" (txt_of_expr e1) (txt_of_op op) (txt_of_expr e2)
   | Unop(op, e) -> sprintf "%s%s" (txt_of_op op) (txt_of_expr e)
-  | _ -> ""
+  | Call(f, args) -> txt_of_func_call f args
 
 and txt_of_func_call f args = match f with
   | "print" -> sprintf "print(%s)" (txt_of_args args)
