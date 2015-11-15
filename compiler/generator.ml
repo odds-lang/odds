d78fdcac6abb21f2fbca64f6c843287e94685622
Alexandra Medway <afm2134@columbia.edu>
2015-11-15 16:18:08 -0500

    Initial function creation

--- compiler/generator.ml
+++ compiler/generator.ml
@@ -15,7 +15,7 @@ module StringMap = Map.Make(String)
 
 let ss_counter = ref (-1) (* Static Scoping Variable Counter *)
 
-let get_ss_id name = 
+let get_ss_id name =
   ss_counter := !ss_counter + 1;
   sprintf "%s_%d" name !ss_counter
 
@@ -42,17 +42,16 @@ let rec txt_of_expr env = function
   | String_lit(s) -> env, sprintf "\"%s\"" s
   | Bool_lit(b) -> env, String.capitalize (string_of_bool(b))
   | Id(e) -> env, if StringMap.mem e env then StringMap.find e env else e
-  | Unop(op, e) -> let _, e = txt_of_expr env e in 
+  | Unop(op, e) -> let _, e = txt_of_expr env e in
       env, sprintf "(%s%s)" (txt_of_op op) e
-  | Binop(e1, op, e2) -> 
+  | Binop(e1, op, e2) ->
       let _, e1 = txt_of_expr env e1 and _, e2 = txt_of_expr env e2 in
       env, sprintf "(%s %s %s)" e1 (txt_of_op op) e2
   | Call(f, args) -> let _, id = txt_of_expr env f in
       env, txt_of_func_call env id args
-  | Assign(id, e) ->
-      let ss_id = get_ss_id id and _, e = txt_of_expr env e in
-      StringMap.add id ss_id env, sprintf "%s = %s" ss_id e
+  | Assign(id, e) -> txt_of_assign env id e
   | List(l) -> let e = txt_of_list env l in env, sprintf "[%s]" e
+  | Fdecl(f) -> txt_of_fdecl env "anon" f
 
 (* Function calls *)
 and txt_of_func_call env f args = match f with
@@ -64,19 +63,40 @@ and txt_of_list env = function
   | [x] -> snd (txt_of_expr env x)
   | _ as l -> String.concat ", " (List.map (fun e -> snd (txt_of_expr env e)) l)
 
+and txt_of_fdecl env id f =
+    let ss_id = get_ss_id id in
+    let new_env = StringMap.add id ss_id env in
+    let list_txt = txt_of_list new_env f.params in
+    let ret_env, body_txt = txt_of_stmts new_env f.body in
+    let _, ret_txt = txt_of_expr ret_env f.return in
+    let func_txt = sprintf "def %s(%s):
+        \n{
+          \n%s
+          \nreturn %s
+        \n}
+      \n" ss_id list_txt body_txt ret_txt in
+    new_env, func_txt
+
+and txt_of_assign env id = function
+  | Fdecl(f) -> txt_of_fdecl env id f
+  | _ as e ->
+      let ss_id = get_ss_id id and _, e = txt_of_expr env e in
+      StringMap.add id ss_id env, sprintf "%s = %s" ss_id e
+
+
 (* Statements *)
-let txt_of_stmt env = function
+and txt_of_stmt env = function
   | Do(e) -> let env, e = txt_of_expr env e in env, sprintf "%s" e
 
-let txt_of_stmts stmt_list = 
+and txt_of_stmts env_input stmt_list =
   let rec process_stmts env acc = function
-    | [] -> String.concat "\n" (List.rev acc)
+    | [] -> env, String.concat "\n" (List.rev acc)
     | stmt :: tl -> let updated_env, stmt_txt = txt_of_stmt env stmt in
         process_stmts updated_env (stmt_txt :: acc) tl
-  in process_stmts StringMap.empty [] stmt_list
+  in process_stmts env_input [] stmt_list
 
 (* Code generation entry point *)
 let gen_program output_file program =
-  let code = txt_of_stmts program in 
+  let _, code = txt_of_stmts StringMap.empty program in
   let file = open_out output_file in
   fprintf file "%s\n" code; close_out file
