9505dccd006224cd6323ef4a7f29a4fcf586bafe
Alexandra Medway <afm2134@columbia.edu>
2015-11-22 17:19:37 -0500

    Finished failed test script

--- test/fail_test.sh
+++ test/fail_test.sh
@@ -6,36 +6,21 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 
 INPUT_FILES="compiler/fail/*.ods"
-TMP_FILE=$(mktemp "compiled.XXXXX")
+TMP_FILE="temp.py"
 printf "${CYAN}Running compiler tests...\n${NC}"
 
 for input_file in $INPUT_FILES; do
-    output_file=${input_file/.ods/.out}
+    expected_error_file=${input_file/.ods/.out}
+    exp_err=$(tr -d "[:space:]" < $expected_error_file)
+    error=$(../compiler/odds.sh -c $input_file $TMP_FILE 2>&1 | tr -d "[:space:]")
 
-    # compile odds program to temp python file
-    ../compiler/odds.sh -c $input_file $TMP_FILE
-
-    # if python test file exists, compare them
-    if [ -e "$python_file" ]; then
-        cmp -s $python_file $TMP_FILE
-        if [ "$?" -ne 0 ]; then
-            printf "%-65s ${RED}ERROR\n${NC}" "  - checking $python_file..." 1>&2
-            rm -f $TMP_FILE
-            exit 1
-        fi
+    if [[ "$exp_err" == "$error" ]]; then
+        printf "%-65s ${GREEN}SUCCESS\n${NC}" "  - checking $input_file..."
+    else
+        printf "%-65s ${RED}ERROR\n${NC}" "  - checking $input_file..." 1>&2
+        exit 1
     fi
 
-    # if test output file exists, compare compiled output to it
-    if [ -e "$output_file" ]; then
-        python $TMP_FILE | cmp -s $output_file -
-        if [ "$?" -ne 0 ]; then
-            printf "%-65s ${RED}ERROR\n${NC}" "  - checking $output_file..." 1>&2
-            rm -f $TMP_FILE
-            exit 1
-        fi
-    fi
-
-    printf "%-65s ${GREEN}SUCCESS\n${NC}" "  - checking $input_file..."
 done
 
 rm -f $TMP_FILE
