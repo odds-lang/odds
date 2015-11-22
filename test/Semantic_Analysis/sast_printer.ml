968297f544589ad04624c83fb43067684806f8b3
dannyech <dannyech@gmail.com>
2015-11-22 13:42:17 -0500

    added pattern matchings to str_of_expr (not complete though)

--- test/Semantic_Analysis/sast_printer.ml
+++ test/Semantic_Analysis/sast_printer.ml
@@ -7,27 +7,40 @@
  *  - Daniel Echikson
  *  - Lilly Wang
  *)
+open Ast
 open Sast
 open Analyzer
 open Printf
 
 
-let rec str_of_expr = function
+let str_of_expr = function
   | Num_lit(n) -> 
-  | String_lit(s)
-  | Bool_lit(b)
-  | Unop(op, wrapped_expr)
-  | Binop of expr * Ast.binop * expr
-  | Id of string
-  | Assign of string * expr
-  | Call of expr * expr list
-  | List of expr list
-  | Fdecl of fdecl
+      begin match n with
+        | Ast.Num_int(i) -> string_of_int i
+        | Ast.Num_float(f) -> string_of_float f
+      end
+  | String_lit(s) -> s
+  | Bool_lit(b) -> string_of_bool b
+  | Unop(op, we) -> 
+    let op_str = Sast.str_of_unop op and we_str = str_of_wrapped_expr we in
+    sprintf "%s%s" op_str we_str
+  | Binop(we1, op, we2) -> 
+      let we1_str = str_of_wrapped_expr we1 and
+        op_str = Sast.str_of_unop op and 
+        we2_str = str_of_wrapped_expr we2 in
+      sprintf "%s %s %s" we1_str op_str we2_str
+  | Id(id) -> id
+  | Assign(id, we) -> 
+      let we_str = str_of_wrapped_expr we in 
+      sprintf "%s = %s" id we_str
+  | Call(we, we_list) -> 
+  | List(we_list) -> 
+  | Fdecl(fdecl) -> 
 
 and str_of_wrapped_expr = function
   | Sast.Expr(expr, typ) -> 
       let type_str = Analyzer.str_of_type typ and expr_str = str_of_expr expr in
-      sprintf "%s %s" type_str expr_str
+      sprintf "{%s %s}" type_str expr_str
 
 let str_of_stmt = function
   | Sast.Do(expr_wrapper) -> 
