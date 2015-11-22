12504cfac5fe199625b4a9a16234dc09d600cb57
Alex Kalicki <alexander.kalicki@gmail.com>
2015-11-22 14:48:01 -0500

    Revert "Updated sast print to account for the fact that we are bubbling up type data in the analyzer."
    This reverts commit 1b4f58dcaef3a477154934cfc3ec28ce2155ad8d.

--- test/Semantic_Analysis/sast_printer.ml
+++ test/Semantic_Analysis/sast_printer.ml
@@ -13,8 +13,8 @@ open Analyzer
 open Printf
 
 (* Utility Functions *)
-let str_of_expr_list l = 
-  String.concat ", " (List.map str_of_expr l)
+let str_of_wrapped_expr_list l = 
+  String.concat ", " (List.map str_of_wrapped_expr l)
 
 (* Stringerizer *)
 and str_of_expr = function
@@ -25,29 +25,29 @@ and str_of_expr = function
       end
   | String_lit(s) -> s
   | Bool_lit(b) -> string_of_bool b
-  | Unop(op, e) -> 
-    let op_str = Sast.str_of_unop op and e_str = str_of_expr e in
-    sprintf "%s%s" op_str e_str
-  | Binop(e1, op, e2) -> 
-      let e1_str = str_of_expr e1 and
+  | Unop(op, we) -> 
+    let op_str = Sast.str_of_unop op and we_str = str_of_wrapped_expr we in
+    sprintf "%s%s" op_str we_str
+  | Binop(we1, op, we2) -> 
+      let we1_str = str_of_wrapped_expr we1 and
         op_str = Sast.str_of_binop op and 
-        e2_str = str_of_wrapped_expr e2 in
-      sprintf "%s %s %s" e1_str op_str e2_str
+        we2_str = str_of_wrapped_expr we2 in
+      sprintf "%s %s %s" we1_str op_str we2_str
   | Id(id) -> id
-  | Assign(id, e) -> 
-      let e_str = str_of_expr e in 
-      sprintf "%s = %s" id e_str
-  | Call(e, e_list) -> 
-      let func_name = str_of_expr e and 
-        args_txt = str_of_expr_list e_list in
+  | Assign(id, we) -> 
+      let we_str = str_of_wrapped_expr we in 
+      sprintf "%s = %s" id we_str
+  | Call(we, we_list) -> 
+      let func_name = str_of_wrapped_expr we and 
+        args_txt = str_of_wrapped_expr_list we_list in
       sprintf "%s(%s)" func_name args_txt
-  | List(e_list) -> 
-      let l_txt = str_of_expr_list e_list in
+  | List(we_list) -> 
+      let l_txt = str_of_wrapped_expr_list we_list in
       sprintf "[%s]" l_txt
   | Fdecl(fdecl) -> 
-      let params_txt = str_of_expr_list fdecl.params and
+      let params_txt = str_of_wrapped_expr_list fdecl.params and
         body_txt = txt_of_stmts fdecl.body and
-        return_txt = txt_of_expr fdecl.return in
+        return_txt = txt_of_wrapped_expr fdecl.return in
       sprintf "(%s) ->\n%s\nreturn %s" params_txt body_txt return_txt
 
 and str_of_wrapped_expr = function
