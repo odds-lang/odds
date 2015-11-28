9fcccfa57f61b86d9cce25ae1fc497b401e9b990
Alexandra Medway <afm2134@columbia.edu>
2015-11-28 00:18:40 -0500

    Changes suggested by Alex

---
+++ test/compiler/pass/_id_call.py
@@ -0,0 +1,3 @@
+print((40 + 2))
+print("fourty-two")
+print((2 * (2 + 3)))
---
+++ test/compiler/pass/_list.py
@@ -0,0 +1,12 @@
+[]
+[1]
+[1, 2, 3]
+[42., 42.5]
+["hello", "world"]
+[True, False, True]
+print([])
+print([1])
+print([1, 2, 3])
+print([42., 42.5])
+print(["Hello, ", "world!"])
+print([True, False, True])
