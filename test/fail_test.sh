5c9c1bdd0a0c99db05d6cb112f4ceabdd43fb49b
Alex Kalicki <alexander.kalicki@gmail.com>
2015-11-28 01:04:11 -0500

    remove unnecessary tests, char-by-char compare, fail test tmp file

--- test/fail_test.sh
+++ test/fail_test.sh
@@ -6,18 +6,18 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 
 INPUT_FILES="compiler/fail/*.ods"
-TMP_FILE="temp.py"
+TMP_FILE=$(mktemp "compiled.XXXXX")
 printf "${CYAN}Running compiler 'fail' tests...\n${NC}"
 
 for input_file in $INPUT_FILES; do
-    expected_error_file=${input_file/.ods/.out}
-    exp_err=$(tr -d "[:space:]" < $expected_error_file)
-    error=$(../compiler/odds.sh -c $input_file $TMP_FILE 2>&1 | tr -d "[:space:]")
+    output_file=${input_file/.ods/.out}
 
-    if [[ "$exp_err" == "$error" ]]; then
+    ../compiler/odds.sh -c $input_file $TMP_FILE 2>&1 | cmp -s $output_file -
+    if [ "$?" -eq 0 ]; then
         printf "%-65s ${GREEN}SUCCESS\n${NC}" "  - checking $input_file..."
     else
         printf "%-65s ${RED}ERROR\n${NC}" "  - checking $input_file..." 1>&2
+        rm -f $TMP_FILE
         exit 1
     fi
 
