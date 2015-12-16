f0dddcffd3c632f0de8669bcd87f6c0b685b3fe2
dannyech <dannyech@gmail.com>
2015-12-15 23:58:52 -0500

    sat_printer now outputs types to terminal in color - also better printing for anon and caked functions

--- compiler/printer.ml
+++ compiler/printer.ml
@@ -19,6 +19,8 @@ let tabsize = 2
 let tabs = ref 0
 let tab_str () = String.make (!tabs * tabsize) ' '
 
+let str_of_colored_type typ = 
+  sprintf "\x1b[34m%s\x1b[0m" (Analyzer.str_of_type typ)
 
 (* Stringerizer *)
 let rec str_of_expr_wrapper = function
@@ -39,11 +41,11 @@ let rec str_of_expr_wrapper = function
         op_str = Analyzer.str_of_binop op and 
         we2_str = str_of_expr_wrapper we2 in
       sprintf "%s %s %s" we1_str op_str we2_str
-  | Sast.Expr(Id(id), typ) -> let typ_str = Analyzer.str_of_type typ in
+  | Sast.Expr(Id(id), typ) -> let typ_str = str_of_colored_type typ in
       sprintf "%s %s" typ_str id
   | Sast.Expr(Assign(id, we), typ) -> 
       let we_str = str_of_expr_wrapper we and 
-        typ_str = Analyzer.str_of_type typ in 
+        typ_str = str_of_colored_type typ in 
       sprintf "%s %s = %s" typ_str id we_str
   | Sast.Expr(Call(we, we_list), _) -> 
       let func_name = str_of_expr_wrapper we and 
@@ -53,17 +55,15 @@ let rec str_of_expr_wrapper = function
       let l_txt = str_of_expr_wrapper_list we_list in
       sprintf "[%s]" l_txt
   | Sast.Expr(Fdecl(fdecl), typ) -> str_of_fdecl fdecl typ
-  | Sast.Expr(Cake(fdecl_ew, call_ew), _) -> 
-      let call_txt = str_of_expr_wrapper call_ew in
-      sprintf "%s" call_txt
-  | Sast.Expr(If(cond), typ) -> str_of_cond cond typ
+  | Sast.Expr(Cake(fdecl_ew, call_ew), _) -> str_of_cake fdecl_ew call_ew
+  | Sast.Expr(If(cond), _) -> str_of_cond cond
 
 and str_of_expr_wrapper_list l = 
   String.concat ", " (List.map str_of_expr_wrapper l)
 
 and str_of_fdecl fdecl typ = 
   let str_of_param_and_type typ param = 
-    sprintf "%s %s" (Analyzer.str_of_type typ) param in
+    sprintf "%s %s" (str_of_colored_type typ) param in
 
   let func = match typ with
     | Func(func) -> func
@@ -71,41 +71,37 @@ and str_of_fdecl fdecl typ =
 
   tabs := !tabs + 1;
   let params_and_types = List.map2 str_of_param_and_type func.param_types fdecl.params and
-    return_type = Analyzer.str_of_type func.return_type in
+    return_type = str_of_colored_type func.return_type in
   
   let decl_txt = sprintf "%s => %s" (String.concat ", " params_and_types) return_type and
     body_txt = str_of_stmts fdecl.body and
     return_txt = str_of_expr_wrapper fdecl.return in
   
   let f_str = 
-    if fdecl.is_anon then 
-      sprintf "%s %s" (Analyzer.str_of_type typ) fdecl.f_name
-    else
-      sprintf "%s(%s) ->\n%s\n%sreturn %s\n" fdecl.f_name decl_txt
-      body_txt (tab_str ()) return_txt in
+    sprintf "%s(%s) ->\n%s\n%sreturn %s\n" fdecl.f_name decl_txt
+    body_txt (tab_str ()) return_txt in
   tabs := !tabs - 1; f_str
 
-(* 
 (* Currently not using *)
 and str_of_cake fdecl_ew call_ew = 
   tabs := !tabs + 1;
   
-  let fdecl, f_type = match fdecl_ew with
-    | Sast.Expr(Sast.Fdecl(fdecl), typ) -> fdecl, typ
+  let fdecl, f_typ = match fdecl_ew with
+    | Sast.Expr(Sast.Fdecl(fdecl), f_typ) -> fdecl, f_typ
     | _ -> raise (Printer_Error "Dead Code Path") in
   
-  let ftype_txt = Analyzer.str_of_type f_type and
+  let fdecl_txt = str_of_fdecl fdecl f_typ and
     call_txt = str_of_expr_wrapper call_ew in
-  let c_str = sprintf "(%s)" call_txt in
+  let c_str = sprintf "{%s}%s" fdecl_txt call_txt in
   tabs := !tabs - 1; c_str
-*)
 
-and str_of_cond cond typ =
+
+and str_of_cond cond =
   tabs := !tabs + 1;
   let tabins = (tab_str ()) ^ (String.make tabsize ' ') in
   
-  let cond_str = sprintf "%s\n%sif %s then\n%s%s\n%selse\n%s%s" 
-    (str_of_type typ) (tab_str ()) (str_of_expr_wrapper cond.cond) tabins 
+  let cond_str = sprintf "\n%sif %s then\n%s%s\n%selse\n%s%s" 
+    (tab_str ()) (str_of_expr_wrapper cond.cond) tabins 
     (str_of_expr_wrapper cond.stmt_1) (tab_str ()) tabins 
     (str_of_expr_wrapper cond.stmt_2) in
   tabs := !tabs - 1; cond_str
