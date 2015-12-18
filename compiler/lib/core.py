a9fe82d15b099103cb9e11d351494abf2464cd90
lillyfwang <lillyfwang@yahoo.com>
2015-12-17 22:43:59 -0500

    added special printing functionality for dists

--- compiler/lib/core.py
+++ compiler/lib/core.py
@@ -11,6 +11,7 @@ from __future__ import print_function
 import math
 import random
 import sys
+import matplotlib.pyplot as plt
 
 # Odds constants
 EUL = math.e
@@ -25,9 +26,18 @@ def exception(s):
     exit(1)
 
 def print(*args, **kwargs):
-    """Call normal print() function, but return argument that was passed"""
+    """Plot distributions for long lists and call normal print() function,
+    but return argument that was passed"""
+    if type(args[0]) is list and len(args[0]) >= DIST_LENGTH:
+        print_dist(args[0])
+        return str(args[0])
     __builtins__.print(*args, **kwargs)
     return str(args[0])
+
+def print_dist(dist):
+    plt.hist(dist)
+    plt.draw()
+    plt.pause(5)
 
 def make_dist(start, end, f):
     """Return a list generated from dist<min, max> | f"""
