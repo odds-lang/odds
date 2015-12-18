ba8e7ea49283ad9ce1189826f42a4071ff7a435e
lillyfwang <lillyfwang@yahoo.com>
2015-12-18 00:55:41 -0500

    added requirements file

--- config/travis_install.sh
+++ config/travis_install.sh
@@ -16,7 +16,6 @@ esac
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers #camlp4-extra opam
-sudo apt-get install -qq python-matplotlib
 
 #export OPAMYES=1
 #export OPAMVERBOSE=1
